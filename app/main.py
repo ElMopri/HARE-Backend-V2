63953bdeb6132c3a0c27c0bd8323597e73384a17
AlexBl21 <hblancorolon@gmail.com>
2025-05-21 23:44:45 -0500

    ajuste de cors

--- app/main.py
+++ app/main.py
@@ -4,11 +4,21 @@ from app.routers.UsuarioRoute import router as usuario_router
 from app.routers.EstudianteRoute import router as estudiante_router
 from app.routers.CatalogoRoute import router as catalogo_router
 from app.auth.authRoutes import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="HARE Backend",
     description="Backend para el sistema educativo HARE",
     version="2.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Incluir los routers
